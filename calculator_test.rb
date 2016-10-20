d82522ece3cc54234f4cdfcc176ddfa839596a30
Pedro Saratscheff <psaratscheff@gmail.com>
2016-10-20 13:05:27 -0300

    basic test coverage

--- calculator_test.rb
+++ calculator_test.rb
@@ -8,7 +8,7 @@ describe 'Calculator test' do
 
   # Test calculate_answer
 
-  it 'should sum correctly' do
+  it 'should add correctly' do
     a = Random.rand(-100..100)
     b = Random.rand(-100..100)
     assert @calculator.calculate_answer('add', a, b) == a + b
@@ -41,15 +41,51 @@ describe 'Calculator test' do
     end
   end
 
-  it 'should throw input error when not integers as input' do
-    a = false
+  it 'should throw input error when not integers as input STRINGS' do
+    a = 'words'
     b = 'characters%"/)'
-    assert_raises ZeroDivisionError do
+    assert_raises TypeError do
       @calculator.calculate_answer('divide', a, b)
     end
   end
 
-  # Test request_calculation_type
+  it 'should throw input error when not integers as input BOOLEAN' do
+    a = false
+    b = 234
+    assert_raises NoMethodError do
+      @calculator.calculate_answer('divide', a, b)
+    end
+  end
 
+  it 'should throw NoMethodError when unavailable operation' do
+    a = false
+    b = 234
+    assert_raises NoMethodError do
+      @calculator.calculate_answer('some_non_existant_operation', a, b)
+    end
+  end
 
+  # test request_calculation_type
+
+  it 'should return add' do
+    assert @calculator.request_calculation_type(1) == 'add'
+  end
+
+  it 'should return subtract' do
+    assert @calculator.request_calculation_type(2) == 'subtract'
+  end
+
+  it 'should return multiply' do
+    assert @calculator.request_calculation_type(3) == 'multiply'
+  end
+
+  it 'should return divide' do
+    assert @calculator.request_calculation_type(4) == 'divide'
+  end
+
+  it 'should throw NoMethodError as it does not exist' do
+    assert_raises NoMethodError do
+      assert @calculator.request_calculation_type(9999)
+    end
+  end
 end
