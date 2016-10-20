aa54a0c45d90a2acc0264fb78f1153cc583ad2c2
Pedro Saratscheff <psaratscheff@gmail.com>
2016-10-20 12:55:24 -0300

    first tests

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+ruby '2.3.1'
+
+gem 'minitest', '~> 5.9.1'
--- calculator_test.rb
+++ calculator_test.rb
@@ -1,11 +1,55 @@
 require_relative 'calculator'
+require 'minitest/autorun'
 
 describe 'Calculator test' do
   def setup
+    @calculator = Calculator.new
   end
 
-  it 'description' do
-    assertion
+  # Test calculate_answer
+
+  it 'should sum correctly' do
+    a = Random.rand(-100..100)
+    b = Random.rand(-100..100)
+    assert @calculator.calculate_answer('add', a, b) == a + b
   end
+
+  it 'should subtract correctly' do
+    a = Random.rand(-100..100)
+    b = Random.rand(-100..100)
+    assert @calculator.calculate_answer('subtract', a, b) == a - b
+  end
+
+  it 'should multiply correctly' do
+    a = Random.rand(-100..100)
+    b = Random.rand(-100..100)
+    assert @calculator.calculate_answer('multiply', a, b) == a * b
+  end
+
+  it 'should divide correctly' do
+    a = Random.rand(-100..100)
+    b = Random.rand(1..100)
+    assert @calculator.calculate_answer('divide', a, b) == a / b
+    assert @calculator.calculate_answer('divide', a, -b) == a / -b
+  end
+
+  it 'should show error on diving by 0' do
+    a = Random.rand(-100..100)
+    b = 0
+    assert_raises ZeroDivisionError do
+      @calculator.calculate_answer('divide', a, b)
+    end
+  end
+
+  it 'should throw input error when not integers as input' do
+    a = false
+    b = 'characters%"/)'
+    assert_raises ZeroDivisionError do
+      @calculator.calculate_answer('divide', a, b)
+    end
+  end
+
+  # Test request_calculation_type
+
 
 end
